091ce9d3fbe43b0ef49a68921693a494508a735d
Julien BY <156668471+julienby@users.noreply.github.com>
2024-04-04 15:58:32 +0200

    Update MyClass.php

--- Libraries/MyLib/src/MyClass.php
+++ Libraries/MyLib/src/MyClass.php
@@ -1,1 +1,23 @@
+<?php
+
+namespace MyLibrary;
+
+class MyClass {
+    
+    private string $name ;
+    
+    public function __construct($name) {
+        $this->name = "default";
+    }
+
+    public function set_name($name) {
+        // Votre logique ici
+        $this->name = $name ;
+        return $this ;
+    }
+    
+    public function captitalize() {
+        return "-->" . $this->name . "<--" ;
+    }
+}
 
