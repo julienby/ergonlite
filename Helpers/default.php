d7e2f6b5261b99f0f21ed8fb9d1187653460834c
Julien BY <156668471+julienby@users.noreply.github.com>
2024-04-04 15:55:52 +0200

    Update default.php

--- Helpers/default.php
+++ Helpers/default.php
@@ -1,1 +1,14 @@
+<?php
+
+// Démarre le chronomètre au début du script
+function startTimer(): float {
+    return microtime(true);
+}
+
+// Calcule et retourne le temps écoulé depuis le début du script
+function endTimer(float $startTime): float {
+    return microtime(true) - $startTime;
+}
+
+
 
