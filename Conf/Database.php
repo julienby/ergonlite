fabf2fc5601e1882f22f77b43d31b32c4704c6df
Julien BY <156668471+julienby@users.noreply.github.com>
2024-04-04 15:33:37 +0200

    Update Database.php

--- Conf/Database.php
+++ Conf/Database.php
@@ -1,1 +1,33 @@
+<?php
+// config.php
+namespace App\Config ;
+
+// Dans config.php
+class Database {
+    private static $pdo = null;
+
+    public static function getPDO() {
+        if (self::$pdo === null) {
+            // Initialiser PDO
+            $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__.'/../');
+            $dotenv->load();
+            
+            try {
+                $pdoOptions = [
+                    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+                ];
+                self::$pdo = new \PDO(
+                    "sqlite:" . __DIR__ . DIRECTORY_SEPARATOR . '../Database/' . $_ENV['DB_FILE'],
+                    null,
+                    null,
+                    $pdoOptions
+                );
+            } catch (\PDOException $e) {
+                die('Connection failed: ' . $e->getMessage());
+            }
+        }
+        return self::$pdo;
+    }
+}
 
