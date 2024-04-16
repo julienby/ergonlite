6a726deeadf001db64f2868769a31d315b1d422c
Julien BY <156668471+julienby@users.noreply.github.com>
2024-04-16 16:59:44 +0200

    add picocss

--- app/Views/layouts/welcome.php
+++ app/Views/layouts/welcome.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- Métadonnées, liens CSS, etc. -->
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="color-scheme" content="light dark" />
+    <link  rel="stylesheet"  href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+/>
 </head>
 <body>
     <?php //require_once __DIR__ . '/../partials/navbar.php'; ?>
@@ -14,7 +18,7 @@
 
 
     <!-- Contenu principal -->
-    <main>
+    <main class="container">
         <?=$content?>
     </main>
 
